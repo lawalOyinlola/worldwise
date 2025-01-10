57604f975e5dd682524e0e802f889444ddf426ac
Lawal Oyinlola <oyinlolalawal1705@gmail.com>
2025-01-10 09:55:48 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+// import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import CityList from "./component/CityList";
+import CountryList from "./component/CountryList";
+import City from "./component/City";
+import Form from "./component/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
+
+function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            {/* <Route index 
+          element={<CityList cities={cities} isLoading={isLoading}  />}
+          /> */}
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
+}
+
+export default App;
